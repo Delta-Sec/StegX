ea4d97caf755759eaa290db9794abb418ea1eead
Ayham_Asfoor <126945679+AyhamAsfoor@users.noreply.github.com>
2025-06-09 17:33:12 +0300

    Delete create_cover.py

--- create_cover.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-# Create a simple 100x100 RGB image
-img = Image.new("RGB", (100, 100), color = "blue")
-img.save("/home/ubuntu/stegx_project/cover_image.png")
-
-print("Created cover_image.png")
